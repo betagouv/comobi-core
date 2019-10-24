d483a0ad5b2d9740999580158e2749df1aaaeee1
David Bruant <bruant.d@gmail.com>
2019-10-24 14:47:31 +0200

    Small get positions urls (#62)
    * Découpage de "places" en paquets de 100 pour éviter que l'URL /positions soit trop longue
    
    * Retourne le résultat :-)

--- geography/getPlacesPosition.js
+++ geography/getPlacesPosition.js
@@ -1,8 +1,21 @@
 import {json} from 'd3-fetch'
+import {chunk} from 'lodash-es'
 
 export default function(places){
     console.log('places', places)
-    return json(`/positions?${([...places].map(p => `p=${encodeURIComponent(p)}`).join('&'))}`)
-    .then(positionObject => (new Map(Object.entries(positionObject))))
-}
 
+    // places can be a large set, which in turn may lead to a large URL
+    // splitting the set so we don't run into "too large URL" HTTP errors
+    return Promise.all(chunk([...places], 100).map(somePlaces => {
+        return json(`/positions?${(somePlaces.map(p => `p=${encodeURIComponent(p)}`).join('&'))}`)
+    }))
+    .then(chunksResults => {
+        const entries = []
+        
+        for(const positionsObject of chunksResults){
+            entries.push(...Object.entries(positionsObject))
+        }
+        
+        return new Map(entries);
+    })
+}
