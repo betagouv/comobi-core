b660d784998d2a7a5914f0f6f5fdaa8e53f08d09
David Bruant <davidbruant@protonmail.com>
2020-12-29 08:43:19 +0100

    Validation du nom de la ville (#36)
    * Suppression de code inutilisé
    
    * suppression d'un <div> inutile et commentaire documentant une mauvaise interaction entre styled-components et useState
    
    * Suppression de "validated"
    
    * Symbolisation de l'id de <datalist>
    
    * Validation standard de l'input de la ville
    
    * Conversion à des espaces plutôt que des tabs
    
    * Affichage du bon message d'erreur au survol en cas d'erreur sur une input

--- direct/components/TripRequestEntry.js
+++ direct/components/TripRequestEntry.js
@@ -1,185 +1,101 @@
 import React, { useState } from 'react'
 import htm from 'htm'
-import {
-	ASYNC_STATUS,
-	STATUS_PENDING,
-	STATUS_ERROR,
-	STATUS_VALUE
-} from '../asyncStatusHelpers'
-import { json } from 'd3-fetch'
 import styled from 'styled-components'
+import escapeRegexp from 'escape-string-regexp'
 
 const html = htm.bind(React.createElement)
 
-const searchCity = (input, setOptions) =>
-	json(
-		`https://geo.api.gouv.fr/communes?nom=${input}&fields=nom,code,departement,region&boost=population`
-	)
-		.then(json => setOptions(json))
-		.catch(function(error) {
-			console.error(
-				'Erreur dans la recherche de communes à partir du code postal',
-				error
-			)
-			setOptions([])
-		})
-
-let Input = styled.input`
-	margin: 0 0.6rem 0 0.6rem;
-	border: 3px solid steelblue;
-	border-radius: 0.3rem;
-	padding: 0.2rem 0.3rem;
-	font-size: 110%;
+const cityInputElement = styled.input`
+    margin: 0 0.6rem 0 0.6rem;
+    border-radius: 0.3rem;
+    padding: 0.2rem 0.3rem;
+    font-size: 110%;
 `
 
-const CityInput = ({ label, input, setInput }) => {
-	const [options, setOptions] = useState([])
+const datalistId = "valid-place-names"
 
-	return html`
-		<div>
-			<label>
-				<${styled.strong`
-					display: inline-block;
-					width: 4.5rem;
-				`}>${label}</strong>
-				<${Input}
-					type="text"
-					list="valid-place-names"
-					value=${input.text}
-					onChange=${e => {
-						const value = e.target.value
-						setInput({ text: value, validated: false })
-						if (value.length > 2) searchCity(e.target.value, setOptions)
-						// Vérifier qu'aucune ville n'est exclue : https://fr.wikipedia.org/wiki/Liste_de_toponymes_courts
-					}}
-				/>
-				${input.validated && '✔'}
-			</label>
-		</div>
-	`
+function makeInputPatternFromList(list){
+    return [...list].map(s => escapeRegexp(s)).join('|')
 }
+
+// styledLabel is defined outside of CityInput because if it's defined inside,
+// it interacts badly with React hooks (useState) in a way that defocuses the input
+// after each character is typed
+// This is a band-aid; the root cause has not been found
+const styledLabel = styled.label` display: block; `
+
+const CityInput = ({ label, validPlaceNames, value, setValue }) => {
+    const pattern = makeInputPatternFromList(validPlaceNames)
+    const validationMessage = `Vous devez saisir un de ces lieux : ${validPlaceNames.join(', ')}`
+
+    return html`
+        <${styledLabel}>
+            <${styled.strong`
+                display: inline-block;
+                width: 4.5rem;
+            `}>${label}</strong>
+            <${cityInputElement}
+                type="text"
+                list=${datalistId}
+                pattern=${pattern}
+                value=${value}
+                onChange=${e => {
+                    e.target.setCustomValidity('');
+                    const value = e.target.value
+                    setValue(value)
+                }}
+                onInvalid=${e => {
+                    e.target.setCustomValidity(validationMessage);
+                }}
+                onBlur=${e => {
+                    e.target.checkValidity() // this triggers an 'invalid' event if input is invalid
+                }}
+            />
+        </label>
+    `
+}
+
 export default function TripRequestEntry({
-	tripRequest,
-	validPlaceNames,
-	onTripRequestChange
+    tripRequest,
+    validPlaceNames,
+    onTripRequestChange
 }) {
-	/*
-	To be re-enabled after Dec 28th 2020
-	https://github.com/betagouv/comobi_valberg/issues/31
+    const [origin, setOrigin] = useState( tripRequest.origin )
+    const [destination, setDestination] = useState( tripRequest.destination )
 
-	const [origin, setOrigin] = useState({
-		text: tripRequest.origin,
-		validated: false
-	})
-	const [destination, setDestination] = useState({
-		text: tripRequest.destination,
-		validated: false
-	})
-
-	return html`
-		<${styled.h2`
-			text-align: center;
-			margin: 0 0 1.5rem;
-		`} key="h2">Où allez-vous ?</h2>
-		<form key="form" className="trip-request-entry" onSubmit=${e => {
-			e.preventDefault()
-			onTripRequestChange({
-				origin: origin.text,
-				destination: destination.text
-			})
-			if (typeof _paq !== 'undefined')
-				_paq.push([
-					'trackEvent',
-					'trajets',
-					'recherche',
-					origin.text + ' | ' + destination.text
-				])
-		}}>
-			<datalist id="valid-place-names">
-				${validPlaceNames.map(validPlaceName => {
-					return html`
-						<option key=${validPlaceName} value=${validPlaceName} />
-					`
-				})}
-			</datalist>
-			<section className="geography">
-				<${CityInput} key="départ" label="Départ" input=${origin} setInput=${setOrigin} />
-				<${CityInput}
-					key="arrivée"
-					label="Arrivée"
-					input=${destination}
-					setInput=${setDestination}
-				/>
-			</section>
-			<button type="submit">Rechercher</button>
-		</form>
-	`
-	*/
-	return html`
-		<${styled.h2`
-			text-align: center;
-			margin: 1.5rem 0;
-		`}>azur.comobi sera ouvert aux passagers et passagères le 28 décembre 2020 !</h2>
-		<${styled.p`
-			text-align: center;
-			margin: 1.5rem 0;
-		`}>Pour le moment, les conducteurs et conductrices s'inscrivent...</p>
-	`
-	
+    return html`
+        <${styled.h2`
+            text-align: center;
+            margin: 0 0 1.5rem;
+        `} key="h2">Où allez-vous ?</h2>
+        <form key="form" className="trip-request-entry" onSubmit=${e => {
+            e.preventDefault()
+            onTripRequestChange({ origin, destination })
+        }}>
+            <datalist id=${datalistId}>
+                ${validPlaceNames.map(validPlaceName => {
+                    return html`
+                        <option key=${validPlaceName} value=${validPlaceName} />
+                    `
+                })}
+            </datalist>
+            <section className="geography">
+                <${CityInput} 
+                    key="départ" 
+                    label="Départ"
+                    validPlaceNames=${validPlaceNames}
+                    value=${origin}
+                    setValue=${setOrigin}
+                />
+                <${CityInput}
+                    key="arrivée"
+                    label="Arrivée"
+                    validPlaceNames=${validPlaceNames}
+                    value=${destination}
+                    setValue=${setDestination}
+                />
+            </section>
+            <button type="submit">Rechercher</button>
+        </form>
+    `
 }
-
-const Options = ({ options, onClick }) =>
-	html`
-		<${styled.ul`
-			list-style-type: none;
-			padding-left: 1rem;
-			li {
-				cursor: pointer;
-			}
-		`} style=${{ width: '100%' }}>
-			${options
-				.map(
-					({ nom, departement }) =>
-						html`
-							<li
-								key=${nom + departement}
-								onClick=${() => onClick({ text: nom, validated: true })}
-							>
-								<span> ${nom}</span
-								><span
-									style=${{
-										color:
-											departement && departement.nom === 'Lot'
-												? 'green'
-												: 'grey'
-									}}
-									>${departement ? ' (' + departement.nom + ')' : ''}
-								</span>
-							</li>
-						`
-				)
-				.slice(0, 5)}
-		</ul>
-	`
-
-const RequestStatus = ({ status }) => html`
-	<div className="status">
-		${status === STATUS_PENDING
-			? 'Recherche en cours...'
-			: status === STATUS_ERROR
-			? html`
-					<div
-						style=${{
-							background: '#ffd8d8',
-							padding: '.4rem 1rem',
-							borderRadius: '1rem',
-							margin: '.6rem'
-						}}
-					>
-						Erreur lors de votre recherche. <br />Êtes-vous sûr que ces villes
-						existent ?
-					</div>
-			  `
-			: undefined}
-	</div>
-`
