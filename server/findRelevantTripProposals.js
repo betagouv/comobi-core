4486d659052e921c5af4b0c554956fa15f1bf52d
audrey bramy <audrey.bramy@gmail.com>
2021-03-16 11:37:41 +0100

    add getRelevantTrip function

--- server/findRelevantTripProposals.js
+++ server/findRelevantTripProposals.js
@@ -9,7 +9,7 @@ import "../helpers/typedef.js"
  * @param {Map<string, Position>} positionByPlace
  * @return {(Trip | number)[][]}
  */
-export default (tripRequest, tripProposalsByTrip, positionByPlace)  => {
+function getAdditionnalTimeByTrip(tripRequest, tripProposalsByTrip, positionByPlace) {
 	const proposedTrips = [...tripProposalsByTrip.keys()]
 	const tripDetailsByTrip = computeTripDetails(proposedTrips, tripRequest, positionByPlace);
 	return proposedTrips
@@ -24,3 +24,22 @@ export default (tripRequest, tripProposalsByTrip, positionByPlace)  => {
 			([_1, additionalTime1], [_2, additionalTime2]) => additionalTime1 - additionalTime2
 		)
 }
+
+/**
+ * 
+ * @param {*} tripProposalsByTrip - all trip proposals ordered by trip object
+ * @param {*} trips - all trips object
+ * @param {*} filter - additionnal time filter
+ */
+function getRelevantTrip(tripProposalsByTrip, trips, filter) {
+	return trips
+		.slice(0, 20)
+		.filter(([_, additionalTime]) => filter(additionalTime))
+		.map(([trip]) => {
+			// get all tripProposal corresponding to the object trip {origin, destination}
+			const tripProposals = tripProposalsByTrip.get(trip)
+			return tripProposals
+		}).flat()
+}
+
+export { getAdditionnalTimeByTrip, getRelevantTrip }
