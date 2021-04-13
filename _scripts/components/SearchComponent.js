a574e55a772959706271d45ec206c4dc60562f2a
audrey bramy <audrey.bramy@gmail.com>
2021-04-13 08:55:46 +0200

    remove date time picker

--- _scripts/components/SearchComponent.js
+++ _scripts/components/SearchComponent.js
@@ -13,60 +13,53 @@ export default function SearchComponent({
 }) {
     const [origin, setOrigin] = useState( tripRequest.origin )
     const [destination, setDestination] = useState( tripRequest.destination )
-    const [startDate, setStartDate] = useState(new Date())
-    const [startTime, setStartTime] = useState(new Date())
+
+    // On pourra ajouter une recherche par date et heure
+    // const [startDate, setStartDate] = useState(new Date())
+    // const [startTime, setStartTime] = useState(new Date())
 
     const reverseClicked = () => {
         setDestination(origin)
         setOrigin(destination)
     }
+
     return html `
     <form key="form" className="search-form" onSubmit=${e => {
         e.preventDefault()
         onTripRequestChange({ origin, destination })
     }}>
-        <${CityInput} 
-            key="départ" 
-            label="Départ"
-            validPlaceNames=${validPlaceNames}
-            value=${origin}
-            setValue=${setOrigin}
-        />
+        <div className="input-list">
+            <${CityInput} 
+                key="départ" 
+                label="Départ"
+                validPlaceNames=${validPlaceNames}
+                value=${origin}
+                setValue=${setOrigin}
+            />
+            <img 
+                key="icon"
+                className="laptop-icon" 
+                src="./images/icons/double-arrow.svg"
+                onClick=${reverseClicked}
+            />
+            <${CityInput}
+                key="arrivée"
+                label="Arrivée"
+                validPlaceNames=${validPlaceNames}
+                value=${destination}
+                setValue=${setDestination}
+            />
+            <div className="search-input">
+                <button className="search-btn" type="submit">Rechercher</button>
+            </div>
+        </div>
         <img 
             key="icon"
-            className="icon" 
+            className="mobile-icon" 
             src="./images/icons/double-arrow.svg"
             onClick=${reverseClicked}
         />
-        <${CityInput}
-            key="arrivée"
-            label="Arrivée"
-            validPlaceNames=${validPlaceNames}
-            value=${destination}
-            setValue=${setDestination}
-        />
-        <label className="search-input">
-            <strong>Date</strong>
-            <${DatePicker} 
-                selected=${startDate}
-                onChange=${setStartDate}
-                
-            />
-        </label>
-        <label className="search-input">
-            <strong>Time</strong>
-            <${TimePicker}
-                onChange=${setStartTime}
-                value=${startTime}
-                clockIcon=${null}
-                disableClock=${true}
-                format=${'HH:mm'}
-                className="city-input"
-            />
-        </label>
-        <div className="search-input">
-        <button className="search-btn" type="submit">Rechercher</button>
-        </div>
+        
     </form>
 `
 }
